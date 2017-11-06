8b215bfee999861276b819fbdd51ee8aea87eeff
Jajauma <jajauma@2ch.hk>
2017-11-06 19:32:05 +0300

    Fixed version: templated struct, get rid of default parameter

--- Static.hpp
+++ Static.hpp
@@ -4,16 +4,17 @@
 #include <string>
 
 namespace detail {
-template <typename Unused = void>
+template <typename Unused>
 struct Globals
 {
     static std::string g_str;
 };
 
-template <>
-std::string Globals<>::g_str = "String too long for short string optimization";
+template <typename Unused>
+std::string Globals<Unused>::g_str
+    = "String too long for short string optimization";
 } /* namespace detail */
 
-static auto& g_str = detail::Globals<>::g_str;
+static auto& g_str = detail::Globals<void>::g_str;
 
 #endif /* end of the header guard */
